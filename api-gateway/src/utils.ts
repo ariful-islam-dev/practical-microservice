538b2e7512d0154b084f32b225251a7a04ae309f
ariful4082 <ariful4082@gmail.com>
2024-03-24 23:05:56 +0600

    Done: Request including api-getway with auth, user, email, product, inventory

--- api-gateway/src/utils.ts
+++ api-gateway/src/utils.ts
@@ -1,47 +1,62 @@
-import { Express, Request, Response } from "express"
-import config from './config.json'
-import axios from "axios"
-
+import { Express, Request, Response } from "express";
+import config from "./config.json";
+import axios from "axios";
+import middlewares from "./middlewares";
 
 // Create Handler Function
-export const createHandler =(hostname:string, path:string, method: string)=>{
-    return async (req: Request, res: Response)=>{
-       try{
+export const createHandler = (
+  hostname: string,
+  path: string,
+  method: string
+) => {
+  return async (req: Request, res: Response) => {
+    try {
+      let url = `${hostname}${path}`;
+      req.params &&
+        Object.keys(req.params).forEach((param) => {
+          url = url.replace(`:${param}`, req.params[param]);
+        });
+      const { data } = await axios({
+        method,
+        url,
+        data: req.body,
+        headers: {
+          origin: "http://localhost:8081",
+          "x-user-id": req.headers["x-user-id"] || "",
+          "x-user-email": req.headers["x-user-email"] || "",
+          "x-user-name": req.headers["x-user-name"] || "",
+          "x-user-role": req.headers["x-user-role"] || "",
+        },
+      });
 
-        let url = `${hostname}${path}`;
-        req.params && Object.keys(req.params).forEach(param=>{
-            url = url.replace(`:${param}`, req.params[param])
-        })
-        const {data}= await axios({
-            method,
-            url,
-            data: req.body,
-            headers: {
-                origin: "http://localhost:8081"
-            }
-        })
+      res.json(data);
+    } catch (error) {
+      if (error instanceof axios.AxiosError) {
+        return res
+          .status(error.response?.status || 500)
+          .json(error.response?.data);
+      }
 
-        res.json(data)
-       }catch(error){
-        if(error instanceof axios.AxiosError){
-            return res.status(error.response?.status || 500).json(error.response?.data)
-        }
-        console.log(error)
-
-        return res.status(500).json({message: "Internal Server Error"});
-       }
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-}
+  };
+};
 
-export const configureRoute = (app: Express)=>{
-    Object.entries(config.services).forEach(([_name, service])=>{
-        const hostname = service.url;
-        service.routes.forEach(route=>{
-            route.methods.forEach(method=>{
-                const handler = createHandler(hostname, route.path, method);
-                const endpoint = `/api${route.path}`
-                app[method](endpoint, handler)
-            })
-        })
-    })
-}
+export const getMiddlewares = (names: string[]) => {
+  return names.map((name) => middlewares[name]);
+};
+export const configureRoute = (app: Express) => {
+  Object.entries(config.services).forEach(([_name, service]) => {
+    const hostname = service.url;
+    service.routes.forEach((route) => {
+      route.methods.forEach((method) => {
+        const endpoint = `/api${route.path}`;
+        const middleware = getMiddlewares(route.middlewares);
+        const handler = createHandler(hostname, route.path, method);
+        // console.log(`Adding route ${method.toLowerCase()} ${endpoint}`);
+
+        app[method](endpoint, middleware, handler);
+      });
+    });
+  });
+};
